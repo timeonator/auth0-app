05f760c3f3dabe9877303f1456d850aaa67149d2
timeonator <timeonator@gmail.com>
2021-02-24 00:50:26 -0800

    auth.config initial commit

---
+++ auth.config.js
@@ -0,0 +1,7 @@
+export const authConfig = {
+    domain: 'dev-enct0se6.us.auth0.com',
+    clientId: 'LXzrHteJ8W6cJ1ilskxz2dP1rUC73Rjq'
+    export const authConfig = {
+        domain: 'replace me',
+        clientId: 'replace me'
+      }
